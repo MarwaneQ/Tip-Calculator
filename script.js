cf5e2de2ae63e03ab368ce6ca091edc9647643e0
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-01-02 22:53:36 +0100

    adding js

---
+++ script.js
@@ -0,0 +1,17 @@
+let bill = document.getElementById("bill");
+let tip = document.getElementById("tip")
+
+let butt = document.querySelector('button')
+let ptotal = document.querySelector('.total')
+
+function count() {
+   let billValue = parseFloat(bill.value);
+   let tipValue = parseFloat(tip.value);
+   let perTomoney = (tipValue*billValue)/100;
+   let total = billValue + perTomoney;
+   let span = document.createElement('span')
+   span.textContent=`${total}`
+   ptotal.appendChild(span)
+   span.style.fontWeight = 'bold'
+}
+butt.addEventListener('click',count)
